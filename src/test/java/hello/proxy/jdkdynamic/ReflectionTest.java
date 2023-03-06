7735a3c02a907bbe7d176fdde5a8fe44a5ea5710
core <jinjs6@gmail.com>
2023-03-06 12:38:54 +0900

    test: 기본적인 리플랙션 테스트

---
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,79 @@
+package hello.proxy.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ReflectionTest {
+
+    @Test
+    void reflection0() {
+        Hello target = new Hello();
+
+        //공통 로직1 시작
+        log.info("start");
+        String result1 = target.callA(); //호출 메서드가 다름
+        log.info("result={}", result1);
+        //공통 로직1 종료
+
+        //공통 로직2 시작
+        log.info("start");
+        String result2 = target.callB(); //호출 메서드가 다름
+        log.info("result={}", result2);
+        //공통 로직2 종료
+    }
+
+    @Test
+    void reflection1() throws Exception {
+        //클래스 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        //callA 정보
+        Method methodA = classHello.getMethod("callA");
+        Object result1 = methodA.invoke(target);
+        log.info("result={}", result1);
+
+        //callB 정보
+        Method methodB = classHello.getMethod("callB");
+        Object result2 = methodB.invoke(target);
+        log.info("result={}", result2);
+    }
+
+    @Test
+    void reflection2() throws Exception {
+        //클래스 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        //callA 정보
+        Method methodA = classHello.getMethod("callA");
+        dynamicCall(methodA, target);
+
+        //callB 정보
+        Method methodB = classHello.getMethod("callB");
+        dynamicCall(methodB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws InvocationTargetException, IllegalAccessException {
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result={}", result);
+    }
+
+    @Slf4j
+    static class Hello {
+        public String callA() {
+            log.info("callA");
+            return "A";
+        }
+
+        public String callB() {
+            log.info("callB");
+            return "B";
+        }
+    }
+}
